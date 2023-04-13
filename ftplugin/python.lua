e60a51703ba0e9705b8f0dfb7bbf7af9044c06ce
Popco35 <78812226+Popco35@users.noreply.github.com>
2023-04-13 01:41:00 -0400

    add save on debug for python

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -4,4 +4,26 @@ local dapp = require('dap-python')
 dapp.setup(home .. "AppData\\Local\\nvim-data\\mason\\packages\\debugpy\\venv\\Scripts\\python.exe")
 
 
-vim.keymap.set('n','<F5>', require('dap').continue, {desc = "start debug"})
+
+
+local debug_session_active = false;
+
+
+dapp.listeners.after.event_initialized["debug_on_save"] = function()
+  debug_session_active = true
+end
+dapp.listeners.before.event_terminated["debug_on_save"] = function()
+  debug_session_active = false
+end
+dapp.listeners.before.event_exited["debug_on_save"] = function()
+  debug_session_active = false
+end
+
+local function setup_debug()
+  if debug_session_active == false then
+    vim.cmd('write')
+  end
+  require('dap').continue()
+end
+
+vim.keymap.set('n','<F5>', setup_debug, {desc = "start debug"})
