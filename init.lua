b6621fd7551f9fe0cdb4cad6f96599866c3fd4dc
OlivierBMA <78812226+Popco35@users.noreply.github.com>
2023-09-18 14:57:39 -0400

    Added luasnip snippets

--- init.lua
+++ init.lua
@@ -273,29 +273,6 @@ require("neodev").setup({
 vim.keymap.set('n', '<C-i>', vim.lsp.buf.format, { desc = 'format the current buffer' })
 
 
--- Setup mason so it can manage external tooling
-require('mason').setup()
-
--- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
-
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
-  end,
-
-  ["jdtls"] = function()
-
-  end
-}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -10,6 +10,50 @@ return {
 		-- nvim-cmp setup
 		local cmp = require 'cmp'
 		local luasnip = require 'luasnip'
+		local ls = require("luasnip")
+		local s = ls.snippet
+		local sn = ls.snippet_node
+		local isn = ls.indent_snippet_node
+		local t = ls.text_node
+		local i = ls.insert_node
+		local f = ls.function_node
+		local c = ls.choice_node
+		local d = ls.dynamic_node
+		local r = ls.restore_node
+		local events = require("luasnip.util.events")
+		local ai = require("luasnip.nodes.absolute_indexer")
+		local extras = require("luasnip.extras")
+		local l = extras.lambda
+		local rep = extras.rep
+		local p = extras.partial
+		local m = extras.match
+		local n = extras.nonempty
+		local dl = extras.dynamic_lambda
+		local fmt = require("luasnip.extras.fmt").fmt
+		local fmta = require("luasnip.extras.fmt").fmta
+		local conds = require("luasnip.extras.expand_conditions")
+		local postfix = require("luasnip.extras.postfix").postfix
+		local types = require("luasnip.util.types")
+		local parse = require("luasnip.util.parser").parse_snippet
+		local ms = ls.multi_snippet
+		local k = require("luasnip.nodes.key_indexer").new_key
+
+
+		ls.add_snippets("java", {
+			s("eachfor", {
+				t("Tjis is nice")
+			}),
+			s("println", {
+				t("System.out.println("), i(1, "texte"), t(");")
+			}),
+			s("main", {
+				t({ "public static void main(String[] args) {", "\t" }), i(1), t({ "", "}" })
+			}),
+			s("for", {
+				t("for(int i = 0; i < "), i(1, "variable"), t({ "; i++) {", "\t" }), i(2), t({ "", "}" })
+			})
+
+		})
 
 		luasnip.config.setup {}
 
