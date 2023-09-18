3a8f6efac6d5558305b3400438bc0dc3debf5b11
OlivierBMA <78812226+Popco35@users.noreply.github.com>
2023-09-18 14:58:19 -0400

    added lazygit and toggleterm changes
    changed keymapping to remove conflicts between lazygit and toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -11,7 +11,7 @@ return {
 		}
 
 		vim.keymap.set('n', '<leader>t', ':ToggleTerm <CR>', { desc = "[T]oggle [T]erminal" })
-		vim.keymap.set('t', '<ESC>', [[<C-\><C-n>]],
+		vim.keymap.set('t', '<C-t>', [[<C-\><C-n>]],
 			{ noremap = true, desc = "Go in normal mode when in terminal" })
 	end
 }
