2f53c40e62bda28f3c195d0d46170323e6ade53b
Popco35 <OlivierBMA@videotron.ca>
2023-10-13 20:40:07 -0400

    smalle changes for linux paths

--- ftplugin/typst.lua
+++ ftplugin/typst.lua
@@ -4,10 +4,10 @@ vim.bo.expandtab = true -- always uses spaces instead of tab characters (et).
 vim.bo.softtabstop = 2  -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
 vim.o.pumheight = 7
 vim.opt.colorcolumn = "80"
-vim.cmd('set fileformat=dos')
+vim.cmd('set fileformat=unix')
 
 
-vim.g.typst_pdf_viewer = "SumatraPDF"
+vim.g.typst_pdf_viewer = "evince"
 
 
 require('lspconfig').typst_lsp.setup {
